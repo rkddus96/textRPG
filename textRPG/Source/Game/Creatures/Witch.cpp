1bc8441ce16eb93ec4da392c09e952ac1514c3c7
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-01-13 19:26:15 +0900

    몬스터 멤버변수 초기화
    몬스터의 멤버 변수 초기화
    
    level 인자에 따라 능력치 변할 수 있도록 생성자 변경
    
    생성될 때 art가 출력

--- textRPG/Source/Game/Creatures/Witch.cpp
+++ textRPG/Source/Game/Creatures/Witch.cpp
@@ -1,15 +1,18 @@
 #include "Witch.h"
 
-Witch::Witch()
+Witch::Witch(int level)
+    : Monster("Witch", 100, 20, 5, 20, 100, level)
 {
     //초기화
-    //MonsterImage = GameManager::GetInstance().GetAssetHandler()->GetASKIIArtContainer(EArt::Slime);
+    MonsterImage = GameManager::GetInstance().GetAssetHandler()->GetASKIIArtContainer(EArtList::Witch);
+    CalculateStats(50, 10, 2);
+    DisplayIntroduction();
 }
 
 void Witch::DisplayIntroduction()
 {
     //아트
-
+    GameManager::GetInstance().GetUIManager()->ChangeBasicCanvasArtImage(MonsterImage);
     //정보 출력
     cout << "Name : " << name << endl;
     cout << "Hp : " << CurHp << endl;
