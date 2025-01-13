cc3e992abc36d1f9b61ed7b74bd0768df30358df
meo-young <meoyoung000@naver.com>
2025-01-13 19:18:23 +0900

    전투 시스템 구현
    - ABattle.h : 전투시스템 추상 클래스
    - AutoBattle.h : 자동전투
    - HandBattle : 수동전투

---
+++ textRPG/Source/Game/Battle/AutoBattle.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ABattle.h"
+
+class AutoBattle : public ABattle
+{
+private:
+	void Battle()
+	{
+		// 로그 선언
+		string InfoLog = "자동 전투가 시작되었습니다";
+		wstring InfoLogToW = LogicHelper::StringToWString(InfoLog);
+
+		// 자동 전투가 시작되었습니다 !
+		UI.AddMessageToBasicCanvasEventInfoUI(InfoLogToW);
+
+		// 연출 용도로 잠시 대기
+		Sleep(1000);
+
+		// 전투 종료가 될 때까지 반복
+		while (!bGameFinished)
+		{
+			if (IsPotionEvent())
+				PotionAction();
+			else
+				PlayerAttackAction();
+
+			MonsterAttackAction();
+		}
+	}
+};
