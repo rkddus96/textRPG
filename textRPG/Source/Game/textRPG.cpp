bf93949e79f5f7b5929a1a63d6eada04edeab0fc
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-01-13 11:04:45 +0900

    Update textRPG.cpp

--- textRPG/Source/Game/textRPG.cpp
+++ textRPG/Source/Game/textRPG.cpp
@@ -1,28 +1,6 @@
-﻿#include <iostream>
-#include "LogicHelper.h"
-#include "Character.h"
+﻿﻿#include <iostream>
 
 int main()
 {
-/*
-    // 캐릭터 생성 Test
-    Character& character = Character::GetInstance();
-    character.Display();
-
-    // 레벨업 테스트
-    int i = 0;
-    while (i < 4)
-    {
-        int exp = 0;
-        std::cin >> exp;
-        character.SetExp(exp);
-        if (exp >= 100)
-        {
-            character.LevelUp();
-            
-        }
-        character.Display();
-        i++;
-    }
- */
+    std::cout << "Hello World!\n";
 }
