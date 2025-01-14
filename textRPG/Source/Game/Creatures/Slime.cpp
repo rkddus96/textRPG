a5e64503d42e3f811574726d3ce5b52541778bd9
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-01-14 17:04:49 +0900

    Devil class 추가

--- textRPG/Source/Game/Creatures/Slime.cpp
+++ textRPG/Source/Game/Creatures/Slime.cpp
@@ -21,9 +21,5 @@ void Slime::DisplayIntroduction()
 {
     //아트
     GameManager::GetInstance().GetUIManager()->ChangeBasicCanvasArtImage(MonsterImage);
-    //정보 출력
-    cout << "Name : " << Name << endl;
-    cout << "Hp : " << CurHp << endl;
-    cout << "Power : " << Power << endl;
-    cout << "Defense : " << Defense << endl;
+
 }
