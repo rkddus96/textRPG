d7122011914fecace5ae840e18a95189611804b2
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-01-14 20:51:32 +0900

    🩹 fix shop class
    shop 클래스
    - ManageShop 함수 추가

--- textRPG/Source/Game/Shop.cpp
+++ textRPG/Source/Game/Shop.cpp
@@ -103,9 +103,12 @@ void Shop::SellItem(Character& character, int index)
 }
 
 
-/*
+
 void Shop::ManageShop(Character& character)
 {
+	// 남은 골드 초기화
+	int Gold = character.GetGold();
+	
 	//UI 변수 초기화
 	std::shared_ptr<UIManager> UI = GameManager::GetInstance().GetUIManager();
 	
@@ -116,11 +119,17 @@ void Shop::ManageShop(Character& character)
 	std::string PurchaseLog;
 	std::string SellLog;
 	std::string BackToFirstChoiceLog;
+	std::string GetOutShopLog;
+	std::string WrongChoiceLog;
+	std::string RemainGoldLog;
 	std::wstring WelcomeLogW;
 	std::wstring FirstChoiceOptionsLogW;
 	std::wstring PurchaseLogW;
 	std::wstring SellLogW;
 	std::wstring BackToFirstChoiceLogW;
+	std::wstring GetOutShopLogW;
+	std::wstring WrongChoiceLogW;
+	std::wstring RemainGoldLogW;
 
 
 	// 로그 초기화
@@ -131,52 +140,111 @@ void Shop::ManageShop(Character& character)
      "(3) 나간다.\n";
 
 	PurchaseLog = 
-	    "어떤 물품을 원하시나요? 원하는 번호를 입력해주세요.";
+	    "어떤 물품을 원하시나요? 원하는 번호를 입력해주세요. \n";
+		"0: 뒤로 가기";
 
 	SellLog = 
-	    "어떤 물품을 파실건가요? 파실 물품의 번호를 입력해주세요.";
+	    "어떤 물품을 파실건가요? 파실 물품의 번호를 입력해주세요.\n";
+		"0: 뒤로 가기";
 
 	BackToFirstChoiceLog = 
 	   "첫 번째 선택으로 돌아갑니다.";
+
+	GetOutShopLog =
+		"상점에서 나갑니다. 다음에 또 오세요!";
+
+	WrongChoiceLog =
+		"잘못된 선택입니다. 다시 입력해주세요.";
+
+	RemainGoldLog =
+		"남은 골드: " + std::to_string(Gold);
+
+
+	
 	WelcomeLogW = LogicHelper::StringToWString(WelcomeLog);
 	FirstChoiceOptionsLogW = LogicHelper::StringToWString(FirstChoiceOptionsLog);
 	PurchaseLogW = LogicHelper::StringToWString(PurchaseLog);
 	SellLogW = LogicHelper::StringToWString(SellLog);
 	BackToFirstChoiceLogW = LogicHelper::StringToWString(BackToFirstChoiceLog);
+	GetOutShopLogW = LogicHelper::StringToWString(GetOutShopLog);
+	WrongChoiceLogW = LogicHelper::StringToWString(WrongChoiceLog);
+	RemainGoldLogW = LogicHelper::StringToWString(RemainGoldLog);
 
 
 	// 로그 출력
 	UI->AddMessageToBasicCanvasEventInfoUI(WelcomeLogW);
-
+	
 	while (true) {
 		
-	//	std::cout << WelcomeLog << std::endl;
-		UI->AddMessageToBasicCanvasEventInfoUI(WelcomeLogW);
 	//	std::cout << FirstChoiceOptionsLog;
 		UI->AddMessageToBasicCanvasEventInfoUI(FirstChoiceOptionsLogW);
 
-		int choice;
-		std::cin >> choice;
+		// 입력값 초기화
+		int Choice;
+		int ItemChoice;
+		std::cin >> Choice;
 
-		switch (choice) {
+		switch (Choice) {
 			case 1: // 구매
 			//	std::cout << PurchaseLog << std::endl;
-				UI->AddMessageToBasicCanvasEventInfoUI(PurchaseLogW);
 				// 구매
+				while (true)
+				{
+					UI->AddMessageToBasicCanvasEventInfoUI(PurchaseLogW);
+					// 사용자 소지 돈 초기화
+					Gold = character.GetGold();
+					Display();
+					std::cin >> ItemChoice;
+					if (ItemChoice == 0)
+					{
+						break;
+					}
+					if (ItemChoice > ItemsForSale.size())
+					{
+						UI->AddMessageToBasicCanvasEventInfoUI(WrongChoiceLogW);
+					}
+					else
+					{
+						BuyItem(character, ItemChoice - 1);
+					}
+					
+					
+				}
 				break;
 
 			case 2: // 판매
-			//	std::cout << SellLog << std::endl;
-				UI->AddMessageToBasicCanvasEventInfoUI(SellLogW);
-				// 판매
+			
+				while (true)
+				{
+					UI->AddMessageToBasicCanvasEventInfoUI(SellLogW);
+					Gold = character.GetGold();
+					Display();
+					UI->AddMessageToBasicCanvasEventInfoUI(RemainGoldLogW);
+				
+					std::cin >> ItemChoice;
+					if (ItemChoice == 0)
+					{
+						break;
+					}
+					if (ItemChoice > ItemsForSale.size())
+					{
+						UI->AddMessageToBasicCanvasEventInfoUI(WrongChoiceLogW);
+					}
+					else
+					{
+						SellItem(character, ItemChoice - 1);
+					}
+					
+					
+				}
 				break;
 
 			case 3: // 나가기
-				std::cout << "상점에서 나갑니다. 다음에 또 오세요!" << std::endl;
+				UI->AddMessageToBasicCanvasEventInfoUI(GetOutShopLogW);
 				return;
 
 			default:
-				std::cout << "잘못된 선택입니다. 다시 입력해주세요." << std::endl;
+				UI->AddMessageToBasicCanvasEventInfoUI(WrongChoiceLogW);
 				break;
 		}
 	}
@@ -184,7 +252,7 @@ void Shop::ManageShop(Character& character)
 
 	
 }
-*/
+
 
 
 
@@ -192,11 +260,35 @@ void Shop::ManageShop(Character& character)
 
 void Shop::Display()
 {
-	for (auto item : ItemsForSale)
+	std::shared_ptr<UIManager> UI = GameManager::GetInstance().GetUIManager();
+
+
+	std::string ItemNameLog;
+	std::string ItemPriceLog;
+	std::string	ItemExplanationLog;
+
+	std::wstring ItemNameLogW;
+	std::wstring ItemPriceLogW;
+	std::wstring ItemExplanationLogW;
+
+	ItemNameLogW = LogicHelper::StringToWString(ItemNameLog);
+	ItemPriceLogW = LogicHelper::StringToWString(ItemPriceLog);
+	ItemExplanationLogW = LogicHelper::StringToWString(ItemExplanationLog);
+
+
+	for (int i = 0; i < ItemsForSale.size(); i++)
 	{
-		std::cout << "   Name: " << item->GetName() << std::endl;
-		std::cout << "   Price: " << item->GetPrice() << std::endl;
-		std::cout << "   Explanation: " << item->GetExplanation() << std::endl;
+		ItemNameLog = std::to_string(i + 1) + ". Name: " + ItemsForSale[i]->GetName() +"\n";
+		ItemPriceLog = "  Price: " + std::to_string(ItemsForSale[i]->GetPrice()) + "\n";
+		ItemExplanationLog = "  Explanation: " + ItemsForSale[i]->GetExplanation() + "\n";
+		
+		UI->AddMessageToBasicCanvasEventInfoUI(ItemNameLogW);
+		UI->AddMessageToBasicCanvasEventInfoUI(ItemPriceLogW);
+		UI->AddMessageToBasicCanvasEventInfoUI(ItemExplanationLogW);
+		
+		std::cout << i+1 << ". Name: " << ItemsForSale[i]->GetName() << std::endl;
+		std::cout << "  Price: " << ItemsForSale[i]->GetPrice() << std::endl;
+		std::cout << "  Explanation: " << ItemsForSale[i]->GetExplanation() << std::endl;
 	}
 	
 }
